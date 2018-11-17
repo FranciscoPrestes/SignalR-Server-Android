bc03895852443f658001e09edbcca046a083a8ce
= <=>
2018-11-17 12:19:28 -0300

    Server ASP.NET

---
+++ SignalR Server/SignalR Server/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNet.SignalR;
+
+
+namespace ProfessusAppAdminWeb.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void EnviarMensagem(string apelido, string mensagem)
+        {
+//            Clients.All.publicarMensagem(apelido, mensagem);
+            Clients.All.publicarMensagem(mensagem);
+
+        }
+    }
+}
